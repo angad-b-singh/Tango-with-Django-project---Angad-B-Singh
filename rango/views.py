b1f9dd07e72e853623d265bba494ad93ac183c0a
angad-b-singh <167092412+angad-b-singh@users.noreply.github.com>
2026-02-11 16:31:37 +0000

    Chapter 6: dynamic index and category views

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Top 5 categories, ordered by likes (descending)
+    category_list = Category.objects.order_by('-likes')[:5]
+    # Top 5 pages, ordered by views (descending)
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list,
+    }
 
     return render(request, 'rango/index.html', context=context_dict)
 
